7543b6fa14f239ccd7853bef920bedd7568df322
Thelma Guerra <thelmaguerrag@gmail.com>
2022-12-11 15:34:58 -0300

    took out dispatch for isHomePage and dispatch()

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -4,14 +4,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import Navbar from "scenes/navbar";
 import FriendListWidget from "scenes/widgets/FriendListWidget";
-import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 import { setIsHomePage } from "state";
 
 const ProfilePage = () => {
-  const dispatch = useDispatch();
-
   const [user, setUser] = useState(null);
   const { userId } = useParams();
   const token = useSelector((state) => state.token);
@@ -28,7 +25,6 @@ const ProfilePage = () => {
     );
     const data = await response.json();
     setUser(data);
-    dispatch(setIsHomePage({ isHomePage: false }));
   };
 
   useEffect(() => {
